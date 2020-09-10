fdbc9e21b28b4fe2225146ef424e28967912ec4d
SpaceHead1C <damh17@gmail.com>
2020-09-10 17:03:04 +0300

    browser hello world

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Listening in port :3000")
+
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	http.ListenAndServe(":3000", nil)
 }
