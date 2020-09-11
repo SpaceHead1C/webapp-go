eb9d262812cdb2cc044ae1f08f89f383badf9cca
SpaceHead1C <damh17@gmail.com>
2020-09-11 10:02:43 +0300

    refactoring

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 )
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello, World!<h1>")
+}
+
 func main() {
 	fmt.Println("Listening in port :3000")
 
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
+	http.HandleFunc("/", indexHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
