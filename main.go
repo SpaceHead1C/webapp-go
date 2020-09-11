f3c2384c333aa6f2d0523d0dcd2b7df7baf6ddf1
SpaceHead1C <damh17@gmail.com>
2020-09-11 12:30:35 +0300

    routing to write

--- main.go
+++ main.go
@@ -16,11 +16,23 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", nil)
 }
 
+func writeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	t.ExecuteTemplate(w, "write", nil)
+}
+
 func main() {
 	fmt.Println("Listening in port :3000")
 
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
+
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/write", writeHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
